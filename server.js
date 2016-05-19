2d391cf0a6a22537a79c97652bf74c2682c1316c
root <root@ip-172-31-3-189.us-west-2.compute.internal>
2016-05-19 21:14:07 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+ res.send('Hello world!');
+});
+app.listen(8000, function(){
+ console.log('listening on port 8000');
+});
